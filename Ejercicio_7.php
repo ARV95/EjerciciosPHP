091c5b8f6aa0a0f748150da4520f3d8d8902174b
ARV95 <52905819+ARV95@users.noreply.github.com>
2020-06-02 00:24:26 -0600

    Update Ejercicio_7.php

--- Ejercicio_7.php
+++ Ejercicio_7.php
@@ -30,29 +30,5 @@ echo ‘</tr>’;
 }
 echo ‘<table>’;
 ?>
-    <form id=»form1″ name=»form1″ method=»post» action=»procesar_primos_sucesivos.php»>
-<table width=»460″ border=»0″>
-<tr>
-<td colspan=»2″><div align=»center»><strong>Ingreso número</strong></div></td>
-</tr>
-<tr>
-<td><strong>Ingrese un número:</strong></td>
-<td><label>
-<input type=»text» name=»numero_primo2″ id=»numero_primo2″ />
-</label></td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<td colspan=»2″><div align=»center»>
-<label>
-<input type=»submit» name=»ok» id=»ok» value=»Ok» />
-</label>
-</div></td>
-</tr>
-</table>
-</form>
 </body>
 </html>
