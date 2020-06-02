4e0c1663d2b39207fd13a3697ce50e5eaf16910f
ARV95 <52905819+ARV95@users.noreply.github.com>
2020-06-01 21:37:11 -0600

    Add files via upload

---
+++ ejercicio2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Ejercicios 2</h1>
+
+<?php
+
+//Este es un comentario de linea
+
+/*
+Este es
+Un comentario de multiples
+Lineas
+*/
+
+$NombreClase = "Lenguaje 4";
+
+echo "<html><head><meta charset=\"utf-8\"></head>;
+echo "<body>";
+echo "<h2>Esta es la clase de $NombreClase mañana
