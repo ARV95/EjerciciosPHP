60aa70ebcb7b472be97b23c66f2fcd60ef107884
ARV95 <52905819+ARV95@users.noreply.github.com>
2020-06-02 23:27:39 -0600

    Update Ejercicio_7.php

--- Ejercicio_7.php
+++ Ejercicio_7.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
+</head>
 <body>
     <h2>Ejercicio 7</h2>
     <p>Programa que evalua si un numero ingresado es un numero primo.</p>
-    
     <form action="Numeros.php" method="post">
         Ingrese un numero: <input type="text1"><br><br>
         <input type="submit" name="primo">
     </form>
-
+</body>
+</html>
 
 <?php
     if(isset($_POST["primo"])){
@@ -38,6 +38,3 @@
 
     }
 ?>
-</head>  
-</body>
-</html>
