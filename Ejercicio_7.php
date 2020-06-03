3ac222b1ff9341b9d30d116bbc4e717399ab68a7
ARV95 <52905819+ARV95@users.noreply.github.com>
2020-06-02 23:20:32 -0600

    Update Ejercicio_7.php

--- Ejercicio_7.php
+++ Ejercicio_7.php
@@ -1,39 +1,40 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
-
-<h1>Ejercicio 7</h1>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Ingrese un numero del 1 al 24: <input type="text" name="fname">
-  <input type="submit">
-</form>
-  
-<?php
- //Prime Function
- function fn_prime($number) {
-    $i = 2; $result = TRUE;
-    while($i < $number) {
-        if(!($number%$i)) {
-            $result = FALSE;
-        }
-        $i++;
-    }
-    return $result;
- }
-
-//Declare integer variable...
-    
-$k = 2;
-
-//Start Loop up to any number of your choice for e.g. 200
-while($k < 25) {
-    if(fn_prime($k)) {
-        echo "$k es un numero primo<br/>";
-    } else {
-        echo "$k no es un numero primo!<br/>";
-    }
-    $k++;
-}
-?>
+    <h2>Ejercicio 7</h2>
+    <p>Programa que evalua si un numero ingresado es un numero primo.</p>
+    <form action="Numeros.php" method="post">
+        Ingrese un numero: <input type="text1"><br><br>
+        <input type="submit" name="primo">
+    </form>
 </body>
 </html>
+
+<?php
+    if(isset($_POST["primo"])){
+        $n=$_POST["text1"];
+        for($i=2;$i<=$n/2;$i++){
+            if($n % $i ==0)
+            {
+                $f=1;
+                break;
+            }
+
+        }
+        if($f==0)
+        {
+            echo "$n es primo";
+        }
+        else
+        {
+            echo "$n no es primo";
+        }
+
+
+    }
+?>
