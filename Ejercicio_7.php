de7dd273870330763d36aa65d6ac0dd00cc2af8e
ARV95 <52905819+ARV95@users.noreply.github.com>
2020-06-02 22:16:32 -0600

    Update Ejercicio_7.php

--- Ejercicio_7.php
+++ Ejercicio_7.php
@@ -5,29 +5,22 @@
 <h1>Ejercicio 7</h1>
   
 <?php
-function prima($n){
-
-  for($i=1;$i<=$n;$i++){  //numbers to be checked as prime
-
-          $counter = 0; 
-          for($j=1;$j<=$i;$j++){ //all divisible factors
-
-
-                if($i % $j==0){ 
-
-                      $counter++;
+function getPrimes($finish)
+    {
+        $number = 2;
+        $range = range($number,$finish);
+        $primes = array_combine($range,$range);
+        while($number*$number < $finish){
+            for($i=$number; $i<=$finish; $i+=$number){
+                if($i==$number){
+                    continue;
                 }
-          }
-
-        //prime requires 2 rules ( divisible by 1 and divisible by itself)
-        if($counter==2){
-
-               print $i." is Prime <br/>";
+                unset($primes[$i]);
+            }
+            $number = next($primes);
         }
+        return $primes;
     }
-} 
-
-prima(20);  //find prime numbers from 1-20
 ?>
 </body>
 </html>
