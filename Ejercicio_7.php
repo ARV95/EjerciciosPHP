352fd10b7a44c8e10dacc9824884969d64a0f0fc
ARV95 <52905819+ARV95@users.noreply.github.com>
2020-06-02 23:07:57 -0600

    Update Ejercicio_7.php

--- Ejercicio_7.php
+++ Ejercicio_7.php
@@ -7,18 +7,6 @@
   Ingrese un numero del 1 al 24: <input type="text" name="fname">
   <input type="submit">
 </form>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // collect value of input field
-    $name = $_POST['fname'];
-    if (empty($name)) {
-        echo "Name is empty";
-    } else {
-        echo $name;
-    }
-}
-
   
 <?php
  //Prime Function
